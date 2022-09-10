564e0a1201794e0de0b2329a23a8e987dce558a0
1923k <soolekefuoe2@gmail.com>
2022-09-10 05:57:01 -0700

    2. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,40 +1,20 @@
 /*
- *
- * Task 2 project
+ * C program Task 2
  * By Sole
+ *
  */
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
-/*
- *  main - Prints the last digit of a randomly generated number
- *
- * and whether it is greater than 5, less than 6, or 0.
- *
- * Return: Always 0.
+
+/**
+ *Main - start of execution
  *
  */
-
 int main(void)
 {
-int n;
+char letter;
 
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-if ((n % 10) > 5)
-{
-printf("Last digit of %d is %d and is greater than 5\n",
-n, n % 10);
-}
-else if ((n % 10) < 6 && (n % 10) != 0)
-{
-printf("Last digit of %d is %d and is less than 6 and not 0\n",
-n, n % 10);
-}
-else
-{
-printf("Last digit of %d is %d and is 0\n",
-}
-
+for (letter = 'a'; letter <= 'z'; letter++)
+putchar(letter);
+putchar('\n');
 return (0);
 }
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,1 +1,2 @@
 1. The last digit
+2. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game.  a program that prints the alphabet in lowercase, followed by a new line
