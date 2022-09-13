83532ecee5a6112ec19a9a9b5814d8eca1f0cb13
1923k <soolekefuoe2@gmail.com>
2022-09-13 07:50:56 -0700

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,19 @@
-#include "main.h"
-/**                                                                                                                             
- * main - prints the string "_putchar"      
- *
- * Return: 0 on success.                                                                                                         
- */                                                                                                                             
-int main(void)   
+#include "main.h" 
+/**
+ * main - prints the string "_putchar" 
+ * 
+ * Return: 0 on success.
+ */
+int main(void)
 {
-_putchar('_');       
+_putchar('_');
 _putchar('p');
-_putchar('u'); 
+_putchar('u');
 _putchar('t');
 _putchar('c');
 _putchar('h');
-_putchar('a');   
+_putchar('a');
 _putchar('r');
-_putchar('\n'); 
-return (0);        
-}   
+_putchar('\n');
+return (0);
+}
