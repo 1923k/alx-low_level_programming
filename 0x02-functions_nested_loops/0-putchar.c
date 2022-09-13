02c80b22d724b0188fc6953b2bb209b0dc44c8ce
1923k <soolekefuoe2@gmail.com>
2022-09-13 07:00:37 -0700

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,19 @@
-#include "main.h"
-/**
- * main - prints the string "_putchar" from a character array.
+#include "main.h"                                                                                                                    
+/**                                                                                                                                   
+ * main - prints the string "_putchar" from a character array.      
  *
- * Return: 0 on success.
- */
-int main(void)
-{
-_putchar('_');
-_putcgar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-
-return (0);
-}
+ * Return: 0 on success.                                                                                                         
+ */                                                                                                                             
+int main(void)   
+{                                                                                                                                     
+_putchar('_');       
+_putcgar('p');                                                                                                                        
+_putchar('u'); 
+_putchar('t');                                                                                                                        
+_putchar('c');                                                                                                                        
+_putchar('h');                                                                                                                        
+_putchar('a');   
+_putchar('r');                                                                                                                        
+_putchar('\n'); 
+return (0);        
+}   
