7f04bcd6be39da94d0beb2d1519f9ec478fd25d4
1923k <soolekefuoe2@gmail.com>
2022-09-14 03:38:59 -0700

    0. Multiple mains

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,6 +1,8 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <stdout.h>
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <stdio.h>
 void positive_or_negative(int i);
-#endif
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
+#endif /* HOLBERTON_H */
