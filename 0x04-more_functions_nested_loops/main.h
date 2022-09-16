ba2436d12f4d14deca5f8677917e3f7e802e33b9
1923k <soolekefuoe2@gmail.com>
2022-09-16 12:18:46 -0700

    Task 0

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,22 +1,17 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _holberton_h_
+#define _holberton_h_
 
-/**
- * main - header file for prototype
- * void protoypes (void)
- * int protoypes (int)
- */
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
 void print_numbers(void);
-old print_most_numbers(void);
+void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-void print_number(int n);
 int _putchar(char);
-int _putchar(int);
-int _isdigit(int);
-int mul(int, int);
+void print_number(int n);
 
-#endif /* MAIN_H */
+#endif
