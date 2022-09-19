afebcff80a9e17a598bf5a0a01e3802551f1f263
1923k <soolekefuoe2@gmail.com>
2022-09-19 04:02:12 -0700

    Task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,13 +9,8 @@ int _atoi(char *);
 int main(void)
 {
 int n;
-int a[5];
-int *p;
 
-a[2] = 1024;
-p = &n;
-
-*(p + 5) = 98;
-printf("a[2] = %d\n", a[2]);
+n = _atoi("4");
+printf("%d\n", n);
 return (0);
 }
