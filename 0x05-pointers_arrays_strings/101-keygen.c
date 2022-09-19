5babb3ed2916000224cb579278e3cbf55865ca98
1923k <soolekefuoe2@gmail.com>
2022-09-19 06:31:42 -0700

    Task 11

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,47 +1,22 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
- * infinite_add - adds two numbers
- * @n1: first number
- * @n2: second number
- * @r: buffer for result
- * @size_r: buffer size
- * Return: address of r or 0
+ * main - generates random valid passwords
+ * Return:0 (on success)
  */
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
+int main(void)
 {
-int i, j, k, l, m, n;
-
-for (i = 0; n1[i]; i++);
-for (j = 0; n2[j]; j++);
-if (i > size_r || j > size_r)
+int pass, sum;
+srand(time(NULL));
+sum = 0;
+while (sum <= 2645)
+{
+pass = (rand() % 128);
+sum += pass;
+printf("%c", pass);
+}
+printf("%c", 2772 - sum);
 return (0);
-
-m = 0;
-
-for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
-{
-n = m;
-
-if (i >= 0)
-n += n1[i] - '0';
-if (j >= 0)
-n += n2[j] - '0';
-if (i < 0 && j < 0 && n == 0)
-{
-break;
-}
-m = n / 10;
-r[k] = n % 10 + '0';
-}
-r[k] = '\0';
-if (i >= 0 || j >= 0 || m)
-return (0);
-for (k -= 1, l = 0; l < k; k--, l++)
-{
-m = r[k];
-r[k] = r[l];
-r[l] = m;
-}
-return (r);
 }
