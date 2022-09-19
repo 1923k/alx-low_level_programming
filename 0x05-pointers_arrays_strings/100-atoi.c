1800b9cedfcc0ddedaf2b70d61f75cdbf1cea0db
1923k <soolekefuoe2@gmail.com>
2022-09-19 04:04:43 -0700

    Task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,16 +1,10 @@
 #include <stdio.h>
 /**
- * main - check the code for Holberton School students.
  * _atoi - function that convert a string to an integer.
  * @s: string
  * Always 0.
  */
-int _atoi(char *);
-int main(void)
+int _atoi(char *s)
 {
-int n;
-
-n = _atoi("4");
-printf("%d\n", n);
-return (0);
+return (*s);
 }
