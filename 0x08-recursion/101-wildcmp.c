586ad4bfc5be4d16b257a77e167189fc31aaaf08
1923k <soolekefuoe2@gmail.com>
2022-09-28 10:19:02 -0700

    Task 8

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -15,7 +15,7 @@ if (*s1 == *s2)
 return (wildcmp(s1 + 1, s2 + 1));
 if (*s2 == '*')
 {
-if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0'
+if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
 return (0);
 if (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2))
 return (1);
