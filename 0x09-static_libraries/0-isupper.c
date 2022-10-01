0d5f6bae17d8504af7d0a4a9093e409a3dcda69a
1923k <soolekefuoe2@gmail.com>
2022-10-01 12:40:06 -0700

    Task 0

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - a function that checks for uppercase character
+ * @c: input
+ * Return: 1 if c is uppercase, 0 otherwis
+ */
+int _isupper(int c)
+{
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+else
+return (0);
+}
---
+++ 0x09-static_libraries/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * char *_strcpy - a function that copies the string pointed to by src
+ * @dest: copy to
+ * @src: copy from
+ * Return: d
+ */
+char *_strcpy(char *dest, char *src)
+{
+char *d = dest;
+
+for (; *src != '\0'; src++)
+{
+*dest = *src;
+dest++;
+}
+*dest = *src;
+return (d);
+}
