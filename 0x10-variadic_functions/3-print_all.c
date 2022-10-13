6ba497af2451735e4c9d1ddd39fd216fa1e42741
1923k <soolekefuoe2@gmail.com>
2022-10-13 12:07:54 -0700

    Updated Task

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,11 +1,11 @@
 /**
  * File: 3-print_all.c
- * Auth: Tolulope Fakunle
+ * Auth: Kefuoe Sole
+ * @arg: A list of arguments pointing to
  */
 #include "variadic_functions.h"
 #include <stdio.h>
 #include <stdarg.h>
-
 void print_char(va_list arg);
 void print_int(va_list arg);
 void print_float(va_list arg);
