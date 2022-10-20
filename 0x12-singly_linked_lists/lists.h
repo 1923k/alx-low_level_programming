50d5dd314b5531b9f27248bec9db1b7f6e240999
1923k <soolekefuoe2@gmail.com>
2022-10-20 01:01:14 -0700

    Updated File

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,13 +1,20 @@
-#ifndef _CODING_H_
-#define _CODING_H_
-#include <stdio.h>
+#ifndef LISTS_H
+#define LISTS_H
+
+/*
+ * File: lists.h
+ * Author: Kefuoe Sole
+ * Desc: Header file containing prototypes and definitions for all functions
+ * and types written in the 0x11-singly_linked_lists directory.
+ */
+#include <stdlib.h>
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
  * Description: singly linked list node structure
- * for Coding project
+ * for Holberton project
  */
 typedef struct list_s
 {
@@ -22,5 +29,4 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
-char *_strdup(const char *str);
-#endif
+#endif /* LISTS_H */
