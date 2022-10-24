b153a1eeb1cab982fd7a53195cad5ef2e09e3d5f
1923k <soolekefuoe2@gmail.com>
2022-10-24 11:19:37 -0700

    Task

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,12 +1,8 @@
-#ifndef LISTS_H
-#define LISTS_H
-/*
- * File: lists.h
- * Author: Keguoe Sole
- * Desc: Header file containing prototypes and definitions for all functions
- * and types written in the 0x12-more_singly_linked_lists directory.
- */
+#ifndef LISTS
+#define LISTS
 #include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -35,5 +31,4 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
-
-#endif /* LISTS_H */
+#endif
