f68fd7bfff3218c4ff31ad00a871121b9b85beae
1923k <soolekefuoe2@gmail.com>
2022-10-27 06:56:29 -0700

    Task

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,22 +11,24 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-unsigned int num = 0, mult = 1;
-int len;
+unsigned int ui;
+int len, base_two;
 
-if (b == '\0')
+if (!b)
 return (0);
-
-for (len = 0; b[len];)
-len++;
-
-for (len -= 1; len >= 0; len--)
+ui = 0;
+for (len = 0; b[len] != '\0'; len++)
+;
+for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
 {
 if (b[len] != '0' && b[len] != '1')
+{
 return (0);
-
-num += (b[len] - '0') * mult;
-mult *= 2;
 }
-return (num);
+if (b[len] & 1)
+{
+ui += base_two;
+}
+}
+return (ui);
 }
