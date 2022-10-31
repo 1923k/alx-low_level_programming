0c3cb465d08c9682dd9f47f1abf638ca390dfe16
1923k <soolekefuoe2@gmail.com>
2022-10-31 10:28:05 -0700

    Tasks

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,7 +8,7 @@
  * @letters: size letters
  * Description: Write a function that reads a text file and prints it
  * to the POSIX standard output.
- * Return: the actual number of letters it could read and print, 0 otherwise 
+ * Return: the actual number of letters it could read and print, 0 otherwise
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
